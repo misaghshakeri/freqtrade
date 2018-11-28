e1dcc8b760db03a7bd7afbec87aade967260f22d
misagh <misagh.shakeri@gmail.com>
2018-11-28 19:39:11 +0100

    tail added

--- r.sh
+++ r.sh
@@ -4,3 +4,4 @@
 rm tradesv3_buyeven.sqlite
 rm run.logs
 python3.6 ./freqtrade/main.py -c config.eth.json -s RSIDivergence --db-url sqlite:///tradesv3_buyeven.sqlite > run.logs 2>&1 &
+tail -f run.logs
