5babaa7a5930bf7a1800a33cde0075b8376bd490
misagh <misagh.shakeri@gmail.com>
2018-12-03 23:49:50 +0100

    slippage corrected

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -452,7 +452,7 @@ class FreqtradeBot(object):
         logger.info('passing order... pair: %s, stake_amount: %s, '
                     'buy limit: %s, last candle close: %s, slippage: %s percent.',
                     pair_s, stake_amount, buy_limit, last_candle_close,
-                    ((buy_limit / last_candle_close) / last_candle_close) * 100
+                    ((buy_limit - last_candle_close) / last_candle_close) * 100
                     )
 
         order = self.exchange.buy(pair=pair, ordertype=self.strategy.order_types['buy'],
